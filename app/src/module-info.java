f5c2ffd9cec47d77def6a722db98a551409a5e54
Aleksandr Barmin <Aleksandr_Barmin@epam.com>
2018-08-16 09:33:02 +0300

    Gradle modules were added

--- app/src/module-info.java
+++
@@ -1,6 +0,0 @@
-module epam.techtalk.app {
-    requires epam.techtalk.service;
-    requires epam.techtalk.utils;
-
-    uses com.epam.techtalk.service.GreetingService;
-}
--- impl-advanced/src/module-info.java
+++
@@ -1,7 +0,0 @@
-import com.epam.techtalk.service.advanced.TerminatorGreetingService;
-
-module epam.techtalk.service.impl.advanced {
-    requires epam.techtalk.service;
-
-    provides com.epam.techtalk.service.GreetingService with TerminatorGreetingService;
-}
