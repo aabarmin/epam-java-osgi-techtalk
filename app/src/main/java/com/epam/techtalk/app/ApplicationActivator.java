e4c85cfa210d9e021312bb8a9809eccc4f719bd9
Aleksandr Barmin <Aleksandr_Barmin@epam.com>
2018-08-16 10:46:50 +0300

    It works, everything works

--- app/src/main/java/com/epam/techtalk/app/ApplicationActivator.java
+++ app/src/main/java/com/epam/techtalk/app/ApplicationActivator.java
@@ -1,15 +1,18 @@
 package com.epam.techtalk.app;
 
+import com.epam.techtalk.service.GreetingService;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
 
 public class ApplicationActivator implements BundleActivator {
     @Override
     public void start(BundleContext context) throws Exception {
         System.out.println("Application bundle starting");
 
-//        final ServiceLoader<GreetingService> loader = ServiceLoader.load(GreetingService.class);
-//        CollectionUtils.each(loader, service -> System.out.println(service.getGreetingMessage()));
+        final ServiceReference<?> serviceReference = context.getServiceReference(GreetingService.class.getName());
+        final GreetingService service = (GreetingService) context.getService(serviceReference);
+        System.out.println(service.getGreetingMessage());
     }
 
     @Override
