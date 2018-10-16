be43fd09077af3496321486c0920c5662eb7ac02
Aleksandr Barmin <barmin.alexander@gmail.com>
2018-10-16 21:47:58 +0300

    Useless files were removed Application was fixed

--- app/src/main/java/com/epam/techtalk/app/Application.java
+++ app/src/main/java/com/epam/techtalk/app/Application.java
@@ -1,13 +1,15 @@
 package com.epam.techtalk.app;
 
 import com.epam.techtalk.service.GreetingService;
-import com.epam.techtalk.utils.CollectionUtils;
 
 import java.util.ServiceLoader;
 
 public class Application {
     public static void main(String[] args) {
         final ServiceLoader<GreetingService> loader = ServiceLoader.load(GreetingService.class);
-        CollectionUtils.each(loader, service -> System.out.println(service.getGreetingMessage()));
+        loader.stream()
+                .map(ServiceLoader.Provider::get)
+                .map(GreetingService::getGreetingMessage)
+                .forEach(System.out::println);
     }
 }
